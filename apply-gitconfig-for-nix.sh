19c41579e885fdb727d879c66507a687cef91b7d
John Doe <johndoe@example.com>
2020-10-15 13:58:36 +0500

    Autoconfig

---
+++ apply-gitconfig-for-nix.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+git config --local include.path "../.gitconfig-for-nix"
+
+
+# setting anonymous name and email if not configured by user
+GitUserEmail=$(git config --get user.email)
+if [[ "$GitUserEmail" == "" ]]; then
+    git config --local user.email "johndoe@example.com"
+fi
+GitUserName=$(git config --get user.name)
+if [[ "$GitUserName" == "" ]]; then
+    git config --local user.name "John Doe"
+fi
